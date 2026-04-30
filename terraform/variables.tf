8d51f691fa69c7b3f4fd093219e9e85a42da95a9
Antoine Rousselot <rousselotantoine7@gmail.com>
2026-04-30 12:42:42 +0200

    terraform initialized and structured

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {
+  default = "eu-west-3"
+}
+
+variable "account_id" {
+  description = "Your AWS account ID"
+}
+
+variable "project_name" {
+  default = "recipe-box"
+}
+
+variable "domain_name" {
+  default = "antorou.online"
+}
